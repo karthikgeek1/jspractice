4a7d0403c2de32a89a05f3e2d6d8698459783e71
karthikgeek1 <kjdnskjn>
2022-06-17 19:45:08 +0530

    whileloop

---
+++ q5.js
@@ -0,0 +1,8 @@
+n=parseInt(process.argv[2])
+let i=1
+while(i<=n){
+    if (i%2==0){
+        console.log(i)
+    }
+    i++
+}
---
+++ q6.js
@@ -0,0 +1,7 @@
+a=parseInt(process.argv[2])
+b=parseInt(process.argv[3])
+let i=b
+while(i>=a){
+     console.log(i)
+     i--
+}
