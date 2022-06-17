0d99667616415733b6064abff853fdc6b49a0921
karthikgeek1 <kjdnskjn>
2022-06-17 19:38:27 +0530

    jsforloop

---
+++ q1.js
@@ -0,0 +1,4 @@
+n=parseInt(process.argv[2])
+for(let i=1; i<=n; i++){
+    console.log(i)
+}
---
+++ q3.js
@@ -0,0 +1,5 @@
+a=parseInt(process.argv[2])
+b=parseInt(process.argv[3])
+for(let i=b; i>=a; i--){
+    console.log(i)
+}
